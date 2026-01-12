00e898fb4542d76109707ea22c73b9e6791a4fa4
NewbRangerTom <67010348+NewbRangerTom@users.noreply.github.com>
2026-01-12 15:07:45 -0500

    added comments to functions in towCalcFunc.h and arranged to match order in towCalcFunc.cpp.  added additional files to the .gitignore file.

--- README.txt
+++ README.txt
@@ -1,6 +1,11 @@
-This program is to calculate the actual towing capacity of a truck vs the advertised towing capacity.
+This program is to calculate the actual towing capacity
+of a truck vs the advertised towing capacity.
+
 It is not an offical calculator sponsored by any company.
-This will provide a best guess based on your actual vehicle ratings that you obtain from your vehicle,
-your vehicles owner manual, and any additional information you obtain from the manufacturer.
+
+This will provide a best guess based on your actual vehicle
+ratings that you obtain from your vehicle,
+your vehicles owner manual, and any additional information
+you obtain from the manufacturer.
 
 ****************************************************************************************************
--- towCalcFunc.h
+++ towCalcFunc.h
@@ -1,22 +1,33 @@
 #pragma once
 
 // function prototypes for towCalcFunc cpp
+
+// 1 - Truck Gross Combined Weight Rating minus Truck Gross Vehicle Weight Rating
 double towCap(double, double);
 
+// 2 - Truck Gross Vehicle Weight Rating mius Truck Payload Capacity
 double trkCurbWeight(double, double);
 
-double hitchWeight(double, double);
-
+// 3 - Passenger Weight plus Exterior Cargo plus Interior Cargo
 double totPayLoadNot(double, double, double);
 
-double totPayLoadWit(double, double, double, double);
+// 4 - this is for 12.5% tongue weight, adjust as needed.
+double hitchWeight(double, double);
 
-double trkAvailPayload(double, double);
-
+// 5 - Dry Weight plus Water Weight plus Battery Weight + RV Cargo Weight
 double rvGroEstWeight(double, double, double, double);
 
-double combGVW(double, double);
-
+// 6 - Truck Gross Combined Weight Rating minus Combined Gross Vehicle Weight
 double therRemTowCap(double, double);
 
+// 7 - Truck Curb Weight plus RV Gross Estimated Weight
+double combGVW(double, double);
+
+// 8 -  RV Gross Vehicle Weight Rating minus RV Gross Estimated Weight
 double rvAvGVWR(double, double);
+
+// 9 - Passenger Weight + Ext Cargo + Int Cargo + Hitch Weight
+double totPayLoadWit(double, double, double, double);
+
+// 10 - Truck Payload Capacity minus Total Payload With Trailer (totPayLoadWit)
+double trkAvailPayload(double, double);
